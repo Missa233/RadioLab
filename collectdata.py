c43f88f009eaa5ec9881461927b3d52ecabf0833
Missa233 <missacarlson@berkeley.edu>
2019-02-13 15:13:53 -0800

    Add files via upload

---
+++ collectdata.py
@@ -0,0 +1,30 @@
+import ugradio
+import numpy as np
+import matplitlib.pyplot as plt
+
+
+def collect1st(volt_range):
+    """volt_range: str """
+    first_data = ugradio.pico.capture_data(volt_range, divisor=10, dual_mode=True)
+    np.savetxt('firsthorn', first_data)
+
+    plt.histogram(first_data)
+    plt.savefig('firsthist.pdf')
+
+    return np.loadtxt("firsthorn")
+
+def insert_upper(volt_range):
+    data = ugradio.pico.capture_data(volt_range, divisor=10, dual_mode=True)
+    np.savetxt('uppertestsig', data)
+
+    return np.loadtxt('uppertestsig')
+
+
+def insert_lower(volt_range):
+    data = ugradio.pico.capture_data(volt_range, divisor=10, dual_mode=True)
+    np.savetxt('lowertestsig', data)
+
+    return np.loadtxt('lowertestsig')
+
+
+
